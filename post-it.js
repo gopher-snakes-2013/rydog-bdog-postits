652c42098750fc1e9c80d59f06795f4b6e78fa08
Apprentice <apprentice@dbc-21.local>
2013-11-05 18:12:13 -0800

    add effects to post its

--- post-it.js
+++ post-it.js
@@ -9,6 +9,8 @@ var Board = function(selector) {
   function createPostIt(event) {
     var postIt = new PostIt(event.clientY, event.clientX)
     $boardElem.append(postIt.$elem);
+    postIt.$elem.show("scale", {percent: 100, origin: ['top','left']}, 400);
+    postIt.$elem.removeClass("hidden");
   };
 
   initialize();
@@ -18,16 +20,17 @@ var PostIt = function(yCoord,xCoord) {
   this.$elem = $("#templates > .post-it").clone().css({top: yCoord, left: xCoord});
   this.$elem.resizable();
   this.$elem.draggable({handle: ".header"});
+  this.$elem.addClass("hidden")
   this.$elem.on("click", function(event) { event.stopPropagation(); });
   this.$elem.find("a.delete").on('click', this.removePostIt);
 };
 
 PostIt.prototype.removePostIt = function(event) {
   //console.log("this === event.target :", this === event.target)
+  $(event.target).closest("div.post-it").toggle( "explode",{pieces: 81}, 1000)
   $(event.target).closest("div.post-it").remove()
   event.stopPropagation()
 };
-
 
 $(function() {
   new Board('#board');
